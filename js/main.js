a03522f4031c12548c17ff6152632abe2524726a
Regina Galvez Chavez <reginagalvezchavez@MBP-de-Regina.iptv.microsoft.com>
2016-11-11 00:41:36 -0300

    terminando trabajo

--- js/main.js
+++ js/main.js
@@ -1,52 +1,91 @@
-function mostrarDiv(){
-  //form
-  //input
-  //btn
-  var add= document.getElementById('miDiv');
-  var elForm= document.createElement('div');
-  var elInput=document.createElement('input');
-  var elBoton=document.createElement('button' ); 
-  var mensajeBtn = document.createTextNode('guardar lista');
+function nvaLista() { 
+  var elTablero = document.getElementById("trello");
+  var laLista = document.createElement("div");
+  var elInput = document.createElement("input");
+  var elBoton = document.createElement("button");
+  var qDiga = document.createTextNode("Guardar lista");
 
-  add.appendChild(elForm);
-  elForm.appendChild(elInput);
-  elForm.appendChild(elBoton);
-  elBoton.appendChild(mensajeBtn);
+  elTablero.appendChild(laLista);
+ laLista.appendChild(elInput);
+ laLista.appendChild(elBoton);
+  elBoton.appendChild(qDiga);
 
-  elForm.setAttribute('class','col-md-4');
-  elBoton.setAttribute('onclick','btnGuardar');
-  
-  //poner text y atrubutte in btn
-  elBoton.appendChild(mensajeBtn);
-  elBoton.setAttribute("class"," baja btn btn-primary ");
-  elBoton.setAttribute("onclick"," btnGuardar()");
-}  
+  laLista.setAttribute("class", "lista col-md-3","col-sm-3","col-xs-3");
+  elBoton.setAttribute("onclick", "agregarLista()");
+  elBoton.setAttribute("class","btn btn-success");
+}
 
-function btnGuardar(){
-  var add = document.getElementById('miDiv').lastChild;
-  var escribeme = add.getElementsByTagName('input')[0];
-  var otroInput = add.getElementsByTagName('input')[0].value;
-  var botonDos = add.getElementsByTagName('button')[0];
- 
-  var elementoTexto = document.createElement('h3');
-  var siguienteBtn = document.createElement('button');
-  var divDos=document.createElement('div');
-  var elNodo = document.createTextNode(escribeme);
-  var siguienteNodo = document.createTextNode('agregar');
+function agregarLista() {
+  // html
+  var laListaDos = document.getElementById("trello").lastChild;
+  var elNvoInput = laListaDos.getElementsByTagName("input")[0];
+  var qDiceElInput = laListaDos.getElementsByTagName("input")[0].value;
+  var btnInputTitulo = laListaDos.getElementsByTagName("button")[0];
 
-  elementoTexto.appendChild(elNodo);
-  add.appendChild(elementoTexto);
-  add.appendChild(divDos);
-  add.appendChild(siguienteBtn);
-  siguienteBtn.appendChild(siguienteNodo);
+  var elementouno = document.createElement("h3");
+  var btnTarea = document.createElement("button");
+  var hacerLAsTareas = document.createElement("div");
+  var otroInput = document.createTextNode(qDiceElInput);
+  var otraTarea = document.createTextNode("Añadir Tarea");
 
-  divDos.setAttribute('class','miDiv');  
-  siguienteBtn.setAttribute('onclick','nuevaTarea()');
-  escribeme.setAttribute('class','hidden');
-  botonDos.setAttribute('class','hidden');
+  laListaDos.appendChild(elementouno);
+  elementouno.appendChild(otroInput);
+  laListaDos.appendChild(btnTarea);
+  laListaDos.appendChild(hacerLAsTareas);
+  btnTarea.appendChild(otraTarea);
+
+  hacerLAsTareas.setAttribute("class", "tareas");
+  btnTarea.setAttribute("onclick", "anadirTarea()");
+  btnTarea.setAttribute("class","btn btn-success");
+  elNvoInput.setAttribute("class", "hidden");
+  btnInputTitulo.setAttribute("class", "hidden","btn btn-success");
 
 }
-function nuevaTarea(){
 
+function anadirTarea() {
+  // html
+  var laListaDos = document.getElementById("trello").lastChild;
+  var lasTareas = laListaDos.lastChild;
+
+  var elText = document.createElement("textarea");
+  var elGuardar = document.createElement("button");
+  var mandarGuardar = document.createTextNode("Guardar Tarea");
+
+  lasTareas.appendChild(elText);
+  lasTareas.appendChild(elGuardar);
+  elGuardar.appendChild(mandarGuardar);
+
+  elGuardar.setAttribute("onclick", "guardarTarea()");
+  elGuardar.setAttribute("class","btn btn-success")
+  btnAnadirTarea.setAttribute("class", "hidden btn btn-success");
+}
+
+function guardarTarea() {
+  // html
+  var laListaDos = document.getElementById("trello").lastChild;
+  var lasTareas = laListaDos.lastChild;
+  var textfinal = lasTareas.getElementsByTagName("textarea")[0];
+  var qDiceText = textfinal.value;
+  var btnDiceText = lasTareas.getElementsByTagName("button")[0];
+
+  var nvaTarea = document.createElement("div");
+ var haciaAbajo = document.createElement("input");
+ var elementodos = document.createElement("h4");
+  var btnTarea = document.createElement("button");
+  var elementonodo = document.createTextNode(qDiceText);
+  var otraTarea = document.createTextNode("Añadir Tarea");
+
+  lasTareas.appendChild(nvaTarea);
+  nvaTarea.appendChild(elementodos);
+  nvaTarea.appendChild(btnTarea);
+  elementodos.appendChild(elementonodo);
+  btnTarea.appendChild(otraTarea);
+
+  nvaTarea.setAttribute("class", "tareaNueva");
+  elementodos.setAttribute("class", "pull-left");
+  btnTarea.setAttribute("onclick", "anadirTarea()");
+  btnTarea.setAttribute("class", "pull-right btn btn-success");
+  textareaTarea.classList.add("oculto");
+  btnDiceText.setAttribute("class", "hidden btn btn-success");
 
 }
